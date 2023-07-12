d65980361ebd33a3f717526ce0955ca760f05dbd
fahim <alfahim.ru@gmail.com>
2023-07-12 20:40:06 +0600

    creat greed model

--- dog_app/models.py
+++ dog_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Breed(models.Model):
+    name=models.CharField(max_length=50)
+    size =models.CharField(max_length=50)
+    friendliness=models.IntegerField() 
+    trainability =models.IntegerField()
+    sheddingamount =models.IntegerField()
+    exerciseneeds =models.IntegerField()
+    
